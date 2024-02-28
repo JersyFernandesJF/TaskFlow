ca1712388d3f43c9c2033b87c29a3c33ee2b2c31
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-02-28 06:34:43 +0000

    envirement
    created envirement and tables: users, department, job

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Users(models.Model):
+  UserID = models.UUIDField(default=uuid.uuid4, unique=True, editable = False, primary_key= True, null=False)
+  Name = models.CharField(max_length= 200, null=False)
+  Email = models.EmailField(unique=True, null=False)
+  Password = models.CharField(null=False, max_length= 300)
+  JobID = models.ForeignKey('Job', on_delete=models.CASCADE)
+
+class Job(models.Model):
+  JobID = models.IntegerField(primary_key=True)
+  Name = models.CharField(unique = True, null=False, max_length= 200)
+  DepartmentID = models.ForeignKey('Department', on_delete=models.CASCADE)
+
+class Department(models.Model):
+  
+  DepartmentId = models.IntegerField(primary_key = True)
+  Name = models.CharField(unique=True, null=False, max_length= 200)
+  
