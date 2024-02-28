7bc531ba065350e0150ded87da29379224366326
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-02-28 07:01:31 +0000

    tables implemented: TeamUsers, Tasks MeetingUsersInveted

--- project/models.py
+++ project/models.py
@@ -8,7 +8,7 @@ class Projects(models.Model):
   Team = models.ForeignKey('Team')
 
 class Team(models.Model):
-  TeamID = models.IntegerField(primary_key = True)
+  TeamID = models.UUIDField(default=uuid.uuid1, unique=True, editable = False, primary_key= True, null=False)
   Name = models.CharField(max_length= 200, null=False)
   
 class Meetings(models.Model):
@@ -17,5 +17,17 @@ class Meetings(models.Model):
   Date = models.CharField(max_length= 200, null=False)
 
 class MeetingUsersInevited(models.Model):
-  User = models.ForeignKey('users.models.Users')
-  Meeting = models.ForeignKey('Meetings')
+  User = models.ForeignKey('users.models.Users', null=False)
+  Meeting = models.ForeignKey('Meetings', null=False)
+
+class TeamUsers(models.Model):
+  User = models.ForeignKey('users.models.Users', null=False)
+  Team = models.ForeignKey('Team', null=False)
+
+class Tasks(models.Model):
+  Tittle = models.CharField(max_length = 400, null=False)
+  SubTittle = models.CharField(max_length = 400, null=True)
+  DataStart = models.CharField(max_length = 100, null=False)
+  DataEnd = models.CharField(max_length = 100, null=False)
+  User = models.ForeignKey('users.models.Users', null=True)
+  Project = models.ForeignKey('Projects', null=False)
