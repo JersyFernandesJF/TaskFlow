07e64f9634c3ac5b03640d46c01d4fe7c507d4d6
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-03-10 18:33:38 +0000

    create hash to password and verify hash

--- users/models.py
+++ users/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 import uuid
+import bcrypt
 
 # Create your models here.
 class Users(models.Model):
   UserID = models.UUIDField(default=uuid.uuid4, unique=True, editable = False, primary_key= True, null=False)
   Name = models.CharField(max_length= 200, null=False)
-  Email = models.EmailField(unique=True, null=False)
-  Password = models.CharField(null=False, max_length= 300)
+  Email = models.EmailField(unique=True, null=False, blank=False)
+  Password = models.CharField(null=False, max_length= 300, blank=False)
   Job = models.ForeignKey('Job', on_delete=models.CASCADE, null=False)
+
+  def __str__(self):
+    return self.Name
+  
+  def hash_password(self, password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+  
+  def verify_password(self,input_password, hashed_password):
+    return bcrypt.checkpw(input_password.encode('utf-8'), hashed_password)
 
 class Job(models.Model):
   Name = models.CharField(unique = True, null=False, max_length= 200)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
 
+from users.models import Users, Department, Job
+from users.serializers import UsersSerializer, DepartmentSerializer, JobSerializer
 # Create your views here.
+
